de25f1af8ea89b9acd8bacd0662c360fc5ae1f76
harshithnetha1 <harshithnetha1@gmail.com>
2024-07-21 17:36:38 +0530

    api

--- src/WeatherComponent.jsx
+++ src/WeatherComponent.jsx
@@ -19,7 +19,8 @@ const WeatherComponent = () => {
         return;
       }
 
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 
       axios.get(url)
         .then(response => {
